6db1db30e71fdc283f7b768e0775275016dfea53
ravichandran1383 <contact@bigspire.com>
2018-04-20 10:59:58 +0530

    changes

--- app/webroot/it/templates_c/233a059d2054779b96d3cce69bafca657f445188_0.file.add_billing_hardware_details.tpl.php
+++ app/webroot/it/templates_c/233a059d2054779b96d3cce69bafca657f445188_0.file.add_billing_hardware_details.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2018-04-16 15:42:27
+/* Smarty version 3.1.29, created on 2018-04-19 13:52:06
   from "C:\xampp\htdocs\itassetsvn\itasset\app\webroot\it\templates\add_billing_hardware_details.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5ad4770b2d8934_06266303',
+  'unifunc' => 'content_5ad851aeb7a4e5_86940175',
   'file_dependency' => 
   array (
     '233a059d2054779b96d3cce69bafca657f445188' => 
     array (
       0 => 'C:\\xampp\\htdocs\\itassetsvn\\itasset\\app\\webroot\\it\\templates\\add_billing_hardware_details.tpl',
-      1 => 1523873545,
+      1 => 1524126091,
       2 => 'file',
     ),
   ),
@@ -23,7 +23,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:include/footer_js.tpl' => 1,
   ),
 ),false)) {
-function content_5ad4770b2d8934_06266303 ($_smarty_tpl) {
+function content_5ad851aeb7a4e5_86940175 ($_smarty_tpl) {
 if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdocs\\itassetsvn\\itasset\\app\\webroot\\it\\vendor\\smarty-3.1.29\\libs\\plugins\\function.html_options.php';
 ?>
 
@@ -55,7 +55,7 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 						</li>
 						
 						<li>
-							<a href="list_billing.php">Hardware</a>
+							<a href="list_billing.php">Billing</a>
 								<i class="icon-angle-right"></i>
 						</li>
 						
@@ -82,7 +82,7 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 											<div class="controls field">
 											<select name="hardware_type_id" id="hardware_type_id">
 										<option value="">Select</option>	
-									<?php echo smarty_function_html_options(array('class'=>"input-xlarge",'placeholder'=>'','style'=>"clear:left",'id'=>"license_no",'options'=>$_smarty_tpl->tpl_vars['billingType']->value,'selected'=>$_SESSION['h']['hardware_type_id']),$_smarty_tpl);?>
+									<?php echo smarty_function_html_options(array('class'=>"input-xlarge",'placeholder'=>'','style'=>"clear:left",'id'=>"license_no",'options'=>$_smarty_tpl->tpl_vars['billingType']->value,'selected'=>$_POST['hardware_type_id']),$_smarty_tpl);?>
 
 										</select>
 									<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['hardware_type_idErr']->value;?>
@@ -92,9 +92,9 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 										<div class="control-group">
 		<label for="textfield" class="control-label">Amount <span class="red_star"> *</span></label>
 			<div class="controls field">
-		<input name="color" class="input-xlarge" placeholder="" type="text" id="color" value="<?php echo $_SESSION['h']['color'];?>
+		<input name="amount" class="input-xlarge" placeholder="" type="text" id="amount" value="<?php echo $_POST['amount'];?>
 "/>
-		<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['colorErr']->value;?>
+		<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['amountErr']->value;?>
  </div>
 			</div>
 	</div>
@@ -105,17 +105,22 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 											<label for="textfield" class="control-label">Payment Type <span class="red_star"> *</span></label>
 											
 											<div class="controls field">
-									<?php echo smarty_function_html_options(array('name'=>'status','class'=>"input-xlarge",'placeholder'=>'','style'=>"clear:left",'id'=>"status",'options'=>$_smarty_tpl->tpl_vars['pay_types']->value,'selected'=>$_SESSION['h']['status']),$_smarty_tpl);?>
+											<select name="payment_type"  tabindex="3" style="clear:left" id="payment_type" class="input-xlarge change_payment_type">
+											<option value="">Select</option>
+											<?php echo smarty_function_html_options(array('options'=>$_smarty_tpl->tpl_vars['pay_types']->value,'selected'=>$_POST['payment_type']),$_smarty_tpl);?>
 	
-											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['statusErr']->value;?>
+											</select>
+											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['payment_typeErr']->value;?>
  </div>	
+											<input name="payment_details" class="input-xlarge payment_Validity" placeholder="" type="text" id="payment_details" value="<?php echo $_POST['payment_details'];?>
+"/> 
 										</div>
 										</div>
 										
 										<div class="control-group">
 											<label for="password" class="control-label">Description<span class="red_star"></span></span></label>
 											<div class="controls">
-									<textarea name="description" rows="2" class="input-xlarge" placeholder="" cols="30" id="description"><?php echo $_SESSION['h']['description'];?>
+									<textarea name="description" rows="2" class="input-xlarge" placeholder="" cols="30" id="description"><?php echo $_POST['description'];?>
 </textarea> 
 											
 											
@@ -139,7 +144,7 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 											<div class="controls field">
 											<select name="it_brand_id" id="it_brand_id">
 										<option value="">Select</option>	
-	<?php echo smarty_function_html_options(array('class'=>"input-xlarge",'placeholder'=>'','style'=>"clear:left",'id'=>"it_brand_id",'options'=>$_smarty_tpl->tpl_vars['hw_brand']->value,'selected'=>$_SESSION['h']['it_brand_id']),$_smarty_tpl);?>
+	<?php echo smarty_function_html_options(array('class'=>"input-xlarge",'placeholder'=>'','style'=>"clear:left",'id'=>"it_brand_id",'options'=>$_smarty_tpl->tpl_vars['hw_brand']->value,'selected'=>$_POST['it_brand_id']),$_smarty_tpl);?>
 
 										</select>											
 											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['it_brand_idErr']->value;?>
@@ -149,41 +154,35 @@ if (!is_callable('smarty_function_html_options')) require_once 'C:\\xampp\\htdoc
 	
 																				
 											<div class="control-group">
-											<label for="textfield" class="control-label">Paid Date <span class="red_star"> *</span></label>
+											<label for="textfield" class="control-label">Bill Date <span class="red_star"> *</span></label>
 											<div class="controls field">
 										
-		<input name="model_id" class="input-xlarge datepick" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
+		<input name="bill_date" class="input-xlarge datepick" placeholder="" type="text" id="bill_date" value="<?php echo $_POST['bill_date'];?>
 "/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
+<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['bill_dateErr']->value;?>
 </div>											
 											</div>
 										</div>
 										<div class="control-group">
 											<label for="password" class="control-label">Bill Copy <span class="red_star"> *</span></label>
 											<div class="controls field">
-<!--											<?php echo smarty_function_html_options(array('name'=>'subscription_validity','class'=>"input-small",'placeholder'=>'','style'=>"clear:left",'id'=>"subscription_validity",'options'=>$_smarty_tpl->tpl_vars['subscription_validity']->value,'selected'=>$_SESSION['h']['subscription_validity']),$_smarty_tpl);?>
--->
-											
- <input name="validity_from" class="input-medium dpd1 sValidity" placeholder="Valid From" type="file" id="validity_from" value="<?php echo $_SESSION['h']['validity_from'];?>
+ <input name="bill_copy" class="input-medium dpd1 sValidity" placeholder="Valid From" type="file" id="bill_copy" value="<?php echo $_POST['bill_copy'];?>
 "/> 										
-
-
- 
- 
-											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['subscription_validityErr']->value;?>
- <?php echo $_smarty_tpl->tpl_vars['valid_tillErr']->value;?>
+											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['bill_copyErr']->value;?>
 </div>
 											</div>
 										</div>
-									
-									
+										<div class="control-group">
+											<label for="password" class="control-label">Bill No <span class="red_star"> *</span></label>
+											<div class="controls field">
+											<input name="bill_no" class="input-xlarge" placeholder="" type="text" id="bill_no" value="<?php echo $_POST['bill_no'];?>
+"> 
+											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['bill_noErr']->value;?>
+</div>
+											</div>
+										</div>
 									</div>
 
-
-										
-							
-								
 							</div>
 						</div>
 						
@@ -197,12 +196,9 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 											<label for="textfield" class="control-label">Company Name  <span class="red_star"> *</span></label>
 											
 											<div class="controls field">
-											<input name="model_id" class="input-xlarge" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
+											<input name="company_name" class="input-xlarge" placeholder="" type="text" id="company_name" value="<?php echo $_POST['company_name'];?>
 "/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
-</div>
-									<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['hardware_type_idErr']->value;?>
+									<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['company_nameErr']->value;?>
  </div>
 										</div>
 										</div>
@@ -212,28 +208,17 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 								
 										<div class="control-group">
 											<label for="textfield" class="control-label">Email Id </label>
-											
 											<div class="controls field">
-									<input name="model_id" class="input-xlarge" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
+									<input name="email_id" class="input-xlarge" placeholder="" type="text" id="email_id" value="<?php echo $_POST['email_id'];?>
 "/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
-</div>	
-											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['statusErr']->value;?>
- </div>	
 										</div>
 										</div>
 										
 										<div class="control-group">
 											<label for="password" class="control-label">Contact Number </label>
 											<div class="controls">
-	<input name="model_id" class="input-xlarge" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
+	<input name="company_contact" class="input-xlarge" placeholder="" type="text" id="company_contact" value="<?php echo $_POST['company_contact'];?>
 "/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
-</div>											
-											
-												  
 											</div>
 										</div>
 										
@@ -247,18 +232,11 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 
 <div class="span6">		
      <div class="control-group">
-											<label for="textfield" class="control-label">Contact Person
-
-
+<label for="textfield" class="control-label">Contact Person
 </label>
 											<div class="controls field">
-											<input name="model_id" class="input-xlarge" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
+											<input name="contact_per" class="input-xlarge" placeholder="" type="text" id="contact_per" value="<?php echo $_POST['contact_per'];?>
 "/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
-</div>											
-											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['it_brand_idErr']->value;?>
-</div>
 											</div>
 										</div>
 	
@@ -267,28 +245,15 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 											<label for="textfield" class="control-label">City </label>
 											<div class="controls field">
 										
-		<input name="model_id" class="input-xlarge" placeholder="" type="text" id="model_id" value="<?php echo $_SESSION['h']['model_id'];?>
-"/> 
-<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['model_idErr']->value;
-echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
-</div>											
+		<input name="city" class="input-xlarge" placeholder="" type="text" id="city" value="<?php echo $_POST['city'];?>
+"/> 										
 											</div>
 										</div>
 										<div class="control-group">
 											<label for="password" class="control-label">Address </label>
 											<div class="controls field">
-<!--											<?php echo smarty_function_html_options(array('name'=>'subscription_validity','class'=>"input-small",'placeholder'=>'','style'=>"clear:left",'id'=>"subscription_validity",'options'=>$_smarty_tpl->tpl_vars['subscription_validity']->value,'selected'=>$_SESSION['h']['subscription_validity']),$_smarty_tpl);?>
--->
-											
-									<textarea name="description" rows="2" class="input-xlarge" placeholder="" cols="30" id="description"><?php echo $_SESSION['h']['description'];?>
+									<textarea name="address" rows="2" class="input-xlarge" placeholder="" cols="30" id="address"><?php echo $_POST['address'];?>
 </textarea> 
-
-
- 
- 
-											<div class="errorMsg error"><?php echo $_smarty_tpl->tpl_vars['subscription_validityErr']->value;?>
- <?php echo $_smarty_tpl->tpl_vars['valid_tillErr']->value;?>
-</div>
 											</div>
 										</div>
 									
@@ -300,14 +265,8 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 										
 							<div class="span12">
 										<div class="form-actions">
-											<input onclick="return validate_hd()" type="submit" name="next" id="submit_next" value="Submit" class="btn btn-primary">
-<!--											<input type="submit" name="next" id="submit_next" value="Next" class="btn btn-primary">-->
-											<?php if (!empty($_SESSION['h']['confirm_add'])) {?>  											
-											<input onclick="return validate_hd()" type="submit" name="confirm" id="submit_confirm" value="Confirm" class="btn btn-primary">
-<!--											<input type="submit" name="confirm" id="submit_confirm" value="Confirm" class="btn btn-primary">-->
-											<?php }?>
-											<a href="list_billing.php"><button type="button" val="list_hardware.php" class="jsRedirect btn regCancel"  onclick="return cancelfunction()" >Cancel</button></a>
-											
+											<input onclick="return validate_brand()" type="submit" name="submit" value="Submit" class="btn btn-primary">
+											<a href="list_billing.php"><button type="button" val="list_billing.php" class="jsRedirect btn regCancel" >Cancel</button></a>
 										</div>
 									</div>	
 								
@@ -332,5 +291,32 @@ echo $_smarty_tpl->tpl_vars['EXIST_MSG']->value;?>
 		</div>
 	<input type="hidden" value="/" id="css_root">
 <?php $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:include/footer_js.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
-}
+?>
+
+
+
+<?php echo '<script'; ?>
+ type="text/javascript">
+
+$(document).ready(function(){
+	// function to change the amount
+	$('.change_payment_type').change(function(){ 
+		if($(this).val() == 'other'){
+			$('.payment_Validity').show();
+		}else{
+			$('.payment_Validity').hide();
+		}
+	});
+	
+	if($('.change_payment_type').length > 0){
+		if($('.change_payment_type:selected').val() == 'other'){
+			$('.payment_Validity').show();
+		}else{
+			$('.payment_Validity').hide();
+		}
+	}
+});
+<?php echo '</script'; ?>
+>	
+<?php }
 }
